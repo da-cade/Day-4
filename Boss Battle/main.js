ce5e300d3d9596acf8ff149943ac21524ec2b56b
Dylan Cade <dacade.code@gmail.com>
2022-03-31 14:41:19 -0600

    Initial Commit

---
+++ Boss Battle/main.js
@@ -0,0 +1,36 @@
+
+
+// TODO Part One
+//format page
+//format data
+//draw two characters
+//give them health
+//add marquee classes as unique animations (health bars, pointer actions)
+//on-click activies - reduces boss's health by 5 -value should stop at 0
+  //modify pointer
+  //modify selectability of fields
+//
+
+// TODO Part Two
+// Boss fights back. Hits on an Interval at 3 seconds - damage reduces hero's health by 5
+// NOTE STRETCH -- It can be fun to give the boss a bit of variable damage: either use a random number between two values 
+  //or for better use in a later step, add an attack property to the boss object.
+
+//TODO Part Three
+// Once our boss is defeated, instead of letting them die and locking their health at 0, we want to have the boss "level up".
+  // instead double all the values of the boss (health, and if you did attack value)
+//NOTE Stretch -- add some fun text, alerts, or indications that show how many monsters have been defeated / what level the monster is on
+
+//TODO Part Four
+// Add a value to the monster to represent the 'gold' reward for slaying the monster. 
+  //Value should be stored and displayed.
+
+//TODO Part Five
+//After a few rounds the hero will lose every time. Add a sort of 'health pack' that the hero can buy with gold
+  // this health pack will need to have a specific cost, that the hero can only buy if they have the gold to spend.
+
+//TODO Stretch -- create a companion animal that does additional damage every time the hero attacks. Use a dictionary.
+  //Add a button to the page that will allow the user to purchase this companion when they have enough gold. Flips bool val to purchased
+  //Next, write a conditional to check if the wolf has been purchased to reduce the extra damage from the boss when the hero attacks.
+  //Once you get this working try adding multiple companions to the list for the hero to purchase, mixing up the cost and the damage.
+
